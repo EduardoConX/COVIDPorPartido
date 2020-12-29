9db0a31349ac419ef9a49a9012925a4b614dc6ff
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2020-12-28 18:45:55 -0600

    COVIDPorPartido 2.0

--- CSVToJSON.py
+++ CSVToJSON.py
@@ -8,9 +8,9 @@ try:
     dfDefunciones = pd.read_csv("Casos_Diarios_Estado_Nacional_Defunciones_" + justoAhora.strftime("%Y%m%d") + ".csv")
 except:
     print("Archivos no encontrados")
-#Manual por si falla en encontrar la fecha
-#dfConfirmados = pd.read_csv('./CSVs/Casos_Diarios_Estado_Nacional_Confirmados_20200909.csv')
-#dfDefunciones = pd.read_csv('./CSVs/Casos_Diarios_Estado_Nacional_Defunciones_20200909.csv')
+##Manual por si falla en encontrar la fecha
+#dfConfirmados = pd.read_csv('Casos_Diarios_Estado_Nacional_Confirmados_20200930.csv')
+#dfDefunciones = pd.read_csv('Casos_Diarios_Estado_Nacional_Defunciones_20200930.csv')
 
 #Listas de fechas
 listaFechasConfirmados = []
@@ -28,7 +28,7 @@ for i in range(3, len(dfDefunciones.columns)):
     listaFechasDefunciones.append(columnasDefunciones[i])
 
 #Funcion para generar el diccionario por partido
-def GeneraDiccionarioPorPartido(estados, partido):
+def GeneraDiccionarioPorPartido(estados, partido, semaforo):
     #Casos confirmados
     listaConfirmadosPorPartido = dfConfirmados.loc[dfConfirmados['cve_ent'].isin(estados)].sum().tolist()
     listaConfirmadosDiarios = []
@@ -59,18 +59,28 @@ def GeneraDiccionarioPorPartido(estados, partido):
     for i in range(len(listaFechasDefunciones)):
         diccionarioDefunciones[listaFechasDefunciones[i]] = {"dia": i, "defunciones": listaDefuncionesDiarias[i], "acumuladas": listaDefuncionesAcumuladas[i]}
     
-    diccionarioPartido = {"confirmados": diccionarioConfirmados, "defunciones": diccionarioDefunciones}
+    diccionarioPartido = {"confirmados": diccionarioConfirmados, "defunciones": diccionarioDefunciones, "semaforo": semaforo}
     return diccionarioPartido
 
+#Poner los semaforos manualmente hasta que se encuentre como automatizarlos
+semaforoPES = {"Amarillo": 1}
+semaforoInd = {"Naranja": 1}
+semaforoMC = {"Naranja" :1}
+semaforoMorena = {"Amarillo": 4, "Naranja": 2}
+semaforoPAN = {"Amarillo" : 6, "Naranja": 3}
+semaforoPRD = {"Naranja": 2}
+semaforoPRI = {"Verde": 1, "Amarillo": 5, "Naranja": 6}
+semaforoNacional = {"Verde": 1, "Amarillo": 16, "Naranja": 15}
+
 #Llamada a la funcion/Generar diccionario por partido
-dictPES = GeneraDiccionarioPorPartido(["17"], "PES")
-dictInd = GeneraDiccionarioPorPartido(["19"], "Ind")
-dictMC = GeneraDiccionarioPorPartido(["14"], "MC")
-dictMorena = GeneraDiccionarioPorPartido(["2", "7", "9", "21", "27", "30"], "Morena")
-dictPAN = GeneraDiccionarioPorPartido(["1", "3", "8", "10", "11", "18", "22", "28", "31"], "PAN")
-dictPRD = GeneraDiccionarioPorPartido(["16", "23"], "PRD")
-dictPRI = GeneraDiccionarioPorPartido(["4", "5", "6", "12", "13", "15", "20", "24", "25", "26", "29", "32"], "PRI")
-dictNacional = GeneraDiccionarioPorPartido(["0"], "Nacional")
+dictPES = GeneraDiccionarioPorPartido(["17"], "PES", semaforoPES)
+dictInd = GeneraDiccionarioPorPartido(["19"], "Ind", semaforoInd)
+dictMC = GeneraDiccionarioPorPartido(["14"], "MC", semaforoMC)
+dictMorena = GeneraDiccionarioPorPartido(["2", "7", "9", "21", "27", "30"], "Morena", semaforoMorena)
+dictPAN = GeneraDiccionarioPorPartido(["1", "3", "8", "10", "11", "18", "22", "28", "31"], "PAN", semaforoPAN)
+dictPRD = GeneraDiccionarioPorPartido(["16", "23"], "PRD", semaforoPRD)
+dictPRI = GeneraDiccionarioPorPartido(["4", "5", "6", "12", "13", "15", "20", "24", "25", "26", "29", "32"], "PRI", semaforoPRI)
+dictNacional = GeneraDiccionarioPorPartido(["0"], "Nacional", semaforoNacional)
 
 diccionarioFinal = {
     "Nacional": dictNacional,
